e2b8d1b51b6f2e01baf5b7d3a52cd1535eec22f2
Alif Ridwan Pahlefi <alifridwan364@gmail.com>
2026-02-26 12:21:59 +0700

    Add files via upload

---
+++ src/main/java/com/alif/produk/service/ProdukService.java
@@ -0,0 +1,31 @@
+package com.alif.produk.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alif.produk.model.Produk;
+import com.alif.produk.repository.ProdukRepository;
+
+@Service
+public class ProdukService {
+    @Autowired
+    private ProdukRepository produkRepository;
+
+    public List<Produk> getAllProduk() {
+        return produkRepository.findAll();
+    }
+
+    public Produk getProdukById(Long id) {
+        return produkRepository.findById(id).orElse(null);
+    }
+
+    public Produk saveProduk(Produk produk) {
+        return produkRepository.save(produk);
+    }
+
+    public void deleteProduk(Long id) {
+        produkRepository.deleteById(id);
+    }
+}
